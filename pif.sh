87e4dd421b1f7d213a796f6c9cac719d6d8434af
Franz Schwartau <franz@electromail.org>
2023-12-29 17:56:09 +0100

    Use temporary dir for intermediate files

--- pif.sh
+++ pif.sh
@@ -3,13 +3,19 @@
 # RSS Feed URL
 url="https://sourceforge.net/projects/xiaomi-eu-multilang-miui-roms/rss?path=/xiaomi.eu/Xiaomi.eu-app"
 
+tmp_dir="$(mktemp -d)"
+apk_file="${tmp_dir}/xiaomi.apk"
+extracted_apk="${tmp_dir}/Extractedapk"
+
+trap 'rm -rf "${tmp_dir}"' EXIT
+
 # Fetch RSS feed and extract the last link
 lastLink=$(curl --silent --show-error "${url}" | grep -oP '<link>\K[^<]+' | head -2 | tail -1)
 
 # Output the last link
 curl --silent --show-error --location --output "${apk_file}" "${lastLink}"
 
-apktool d xiaomi.apk -o Extractedapk -f
+apktool d "${apk_file}" -o "${extracted_apk}" -f
 
 # Function to set variable to "null" if empty
 set_to_null() {
@@ -21,14 +27,14 @@ set_to_null() {
 }
 
 # Assign values to variables
-var_MANUFACTURER=$(grep 'MANUFACTURER' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_BRAND=$(grep 'BRAND' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_DEVICE=$(grep 'DEVICE' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_PRODUCT=$(grep 'PRODUCT' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_MODEL=$(grep 'MODEL' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_FINGERPRINT=$(grep 'FINGERPRINT' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_SECURITY_PATCH=$(grep 'SECURITY_PATCH' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
-var_FIRST_API_LEVEL=$(grep 'FIRST_API_LEVEL' Extractedapk/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_MANUFACTURER=$(grep 'MANUFACTURER' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_BRAND=$(grep 'BRAND' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_DEVICE=$(grep 'DEVICE' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_PRODUCT=$(grep 'PRODUCT' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_MODEL=$(grep 'MODEL' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_FINGERPRINT=$(grep 'FINGERPRINT' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_SECURITY_PATCH=$(grep 'SECURITY_PATCH' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
+var_FIRST_API_LEVEL=$(grep 'FIRST_API_LEVEL' ${extracted_apk}/res/xml/inject_fields.xml | sed 's/.*value="\([^"]*\)".*/\1/' | sed 's/" \/>//')
 
 # Set variables to "null" if empty
 var_MANUFACTURER=$(set_to_null "$var_MANUFACTURER")
