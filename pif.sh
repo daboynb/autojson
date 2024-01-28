b7bcd2e55b7e6aad640ec0204bf80dbffc2f4bdb
daboynb <106079917+daboynb@users.noreply.github.com>
2024-01-28 11:37:22 +0100

    Update pif.sh

--- pif.sh
+++ pif.sh
@@ -17,6 +17,7 @@ create_json() {
   "FINGERPRINT": "$(get_value FINGERPRINT)",
   "SECURITY_PATCH": "$(get_value SECURITY_PATCH)",
   "FIRST_API_LEVEL": "$(get_value FIRST_API_LEVEL)"
+  "KERNEL": "Bananaos-perf"
 }
 EOF
 }
