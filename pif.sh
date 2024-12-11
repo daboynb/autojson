b1e30dd2fb70cada68483920d59ffbe5dca9177d
daboynb <106079917+daboynb@users.noreply.github.com>
2024-12-11 20:13:30 +0100

    Update pif.sh

--- pif.sh
+++ pif.sh
@@ -99,6 +99,8 @@ find . -maxdepth 1 -name "*_HTML" -exec rm {} \;
 # Add fields on chiteroman.json
 ./migrate_chiteroman.sh pif.json chiteroman.json
 jq 'del(.BRAND, .PRODUCT, .DEVICE, .RELEASE, .ID, .INCREMENTAL, .TYPE, .TAGS, .spoofProvider, .spoofProps, .spoofSignature, .DEBUG)' chiteroman.json > tmp.json && mv tmp.json chiteroman.json
+echo "cat new json"
+cat chiteroman.json
 
 # Migrate osmosis
 ./migrate_osmosis.sh -s pif.json osmosis.json # This is for strong
